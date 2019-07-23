5887fb253000c8d9a17e23594169bb9c908315d8
Ousmane Diouf <java-89@hotmail.fr>
2019-07-23 03:41:24 +0200

    Ajout des dependences et configuration de la base de données.

---
+++ src/main/java/SpringRemoting/App.java
@@ -0,0 +1,13 @@
+package SpringRemoting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
